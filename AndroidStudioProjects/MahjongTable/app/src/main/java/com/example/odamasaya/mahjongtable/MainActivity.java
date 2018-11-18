e9833f79da7436596fc3fae69b7c6b5fdf1b63b8
小田将也 <odadmsy0630@gmail.com>
2018-11-18 22:42:51 +0900

    点数表示した

--- AndroidStudioProjects/MahjongTable/app/src/main/java/com/example/odamasaya/mahjongtable/MainActivity.java
+++ AndroidStudioProjects/MahjongTable/app/src/main/java/com/example/odamasaya/mahjongtable/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.odamasaya.mahjongtable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView scoreTexts[] = new TextView[4];
+    int[] ids = {R.id.under, R.id.right, R.id.upper, R.id.left};
+    int[] scores = {25000, 25000, 25000, 25000};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int i;
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        for (i = 0; i < scoreTexts.length; i++) {
+            scoreTexts[i] = findViewById(ids[i]);
+            scoreTexts[i].setText(String.valueOf(scores[i]));
+        }
     }
+
+
+
+
+
 }
